fc8b3790d9cbf0061eec762a738f9bde88e95854
Basnucaev <96333647+Basnucaev@users.noreply.github.com>
2022-05-24 17:00:58 +0300

    Update Controller

--- src/main/java/com/abbreviator/controller/AbbreviateController.java
+++ src/main/java/com/abbreviator/controller/AbbreviateController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class AbbreviateController {
     }
 
     @PostMapping("/abbreviate")
-    public ResponseEntity<Link> abbreviateLink(@RequestParam(name = "uri") String uri) {
+    public ResponseEntity<Link> abbreviateLink(@RequestBody String uri) {
         Link abbreviatedLink = abbreviateService.abbreviateURI(uri);
 
         return new ResponseEntity<>(abbreviatedLink, HttpStatus.OK);
